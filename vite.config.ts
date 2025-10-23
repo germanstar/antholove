56f5ceb9dab41ef1fc99fa2d18f7406670cd6680
germanstar <germande005@gmail.com>
2025-10-23 12:05:48 -0500

    final

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/antholove/", // 👈 ¡Esta línea es la importante!
   server: {
     host: "::",
     port: 8080,
